92e943e0c8e2f77a3d47b3e58a29b4273753b9a1
Iago Rodrigues <yagomello16@gmail.com>
2022-06-27 16:31:49 -0300

    feat(Lesson): add active lesson styles and block page access when not available lesson

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,7 +1,9 @@
+import { useEffect } from 'react'
 import { CheckCircle, Lock } from 'phosphor-react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 import { isPast, format } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+import cx from 'classnames'
 
 interface LessonProps {
   title: string
@@ -11,6 +13,9 @@ interface LessonProps {
 }
 
 export const Lesson = (props: LessonProps) => {
+  const { slug } = useParams<{ slug: string }>()
+  const navigate = useNavigate()
+
   const isAvailableLesson = isPast(props.availableAt)
   const availableDateFormatted = format(
     props.availableAt,
@@ -20,14 +25,35 @@ export const Lesson = (props: LessonProps) => {
     }
   )
 
+  const isActiveLesson = slug === props.slug
+
+  useEffect(() => {
+    if (!isAvailableLesson && isActiveLesson) {
+      navigate('/event')
+    }
+  }, [isActiveLesson])
+
   return (
-    <Link className="group" to={`/event/lesson/${props.slug}`}>
+    <Link
+      className={cx('group', { 'disabled-link': !isAvailableLesson })}
+      to={`/event/lesson/${props.slug}`}
+    >
       <span className="text-greay-300">{availableDateFormatted}</span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 transition-colors">
+      <div
+        className={cx(
+          'rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 transition-colors',
+          { 'bg-green-500': isActiveLesson }
+        )}
+      >
         <header className="flex items-center justify-between">
           {isAvailableLesson ? (
-            <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+            <span
+              className={cx('text-sm  font-medium flex items-center gap-2', {
+                'text-white': isActiveLesson,
+                'text-blue-500': !isActiveLesson,
+              })}
+            >
               <CheckCircle size={20} />
               Conteúdo liberado
             </span>
@@ -38,18 +64,27 @@ export const Lesson = (props: LessonProps) => {
             </span>
           )}
 
-          {props.type === 'live' ? (
-            <span className="text-xs rounded px-2 py-[0.125rem] text-green-300 border border-green-300 font-bold">
-              AO VIVO
-            </span>
-          ) : (
-            <span className="text-xs rounded px-2 py-[0.125rem] text-white border border-green-300 font-bold">
-              AULA PRÁTICA
-            </span>
-          )}
+          <span
+            className={cx(
+              'text-xs rounded px-2 py-[0.125rem] text-white border font-bold',
+              {
+                'border-white': isActiveLesson,
+                'border-green-300': !isActiveLesson,
+              }
+            )}
+          >
+            {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
+          </span>
         </header>
 
-        <strong className="text-gray-200 mt-5 block">{props.title}</strong>
+        <strong
+          className={cx(' mt-5 block', {
+            'text-white': isActiveLesson,
+            'text-gray-200': !isActiveLesson,
+          })}
+        >
+          {props.title}
+        </strong>
       </div>
     </Link>
   )
