eb2be559d1afd5e8f27805e2dfaff27af7403fc0
Iago Rodrigues <yagomello16@gmail.com>
2022-06-27 16:29:28 -0300

    chore(apollo): add authorization header and import env variables

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4vsz2e91tv401uk2yn138s8/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`,
+  },
   cache: new InMemoryCache(),
 })
